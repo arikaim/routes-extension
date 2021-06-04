754f18e244aba061902096c697d9412f4758ec14
Arikaim <sales@intersoft.biz>
2021-06-04 19:23:27 +0300

    =

---
+++ README.md
@@ -0,0 +1,17 @@
+## Arikaim CMS Routes manager Extension
+![version: 1.0.0](https://img.shields.io/github/release/arikaim/routes-extension.svg)
+![license: GPL3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+
+Arikaim CMS Routes manager Extension
+
+
+#### Requirements 
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+  
+
+#### Installation
+
+```sh
+composer require arikaim/routes-extension
+```
---
+++ Routes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Extensions\Routes;
+
+use Arikaim\Core\Extension\Extension;
+
+/**
+ * Routes extension
+*/
+class Routes extends Extension
+{
+    /**
+     * Install extension routes, events, jobs
+     *
+     * @return void
+    */
+    public function install()
+    {         
+    }   
+}
